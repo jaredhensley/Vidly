2d3e7d55afa8b3dc078233cdc0b8b43ef7074c4e
JaredHensley <jahensle85@me.com>
2016-08-13 19:01:47 -0500

    viewmodel added

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -14,7 +15,47 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() {Name = "Jared's Big Adventure"};
 
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "Customer 1"},
+                new Customer() {Name = "Customer 2"}
+            };
+
+            var viewmodel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            
+             
+
+            return View(viewmodel);
+        }
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return Content("Id=" + Id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
